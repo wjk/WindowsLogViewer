b5329cfd3823569c78c554e99bbf07d0b225bef9
William Kent <wjk011@gmail.com>
2023-02-14 14:16:02 -0500

    Initial commit of the application project

---
+++ src/WindowsLogViewer/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+// Copyright (c) William Kent and contributors. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace WindowsLogViewer
+{
+    /// <summary>
+    /// The main top-level window for the application.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
