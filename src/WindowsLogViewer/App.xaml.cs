87defa8485255c4f6eb43a53db059e39fc988ae1
William Kent <wjk011@gmail.com>
2023-03-03 19:50:41 -0500

    Re-add new WPF WindowsLogViewer project

---
+++ src/WindowsLogViewer/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) William Kent and contributors. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LogViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/WindowsLogViewer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) William Kent and contributors. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
