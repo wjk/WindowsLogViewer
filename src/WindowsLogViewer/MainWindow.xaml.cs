1ae78b523a25fdfb441213326775a1ecf0912643
William Kent <wjk011@gmail.com>
2023-03-05 13:22:47 -0500

    Collapse unneeded using statements
    These may be added back piecemeal as needed.

--- src/WindowsLogViewer/MainWindow.xaml.cs
+++ src/WindowsLogViewer/MainWindow.xaml.cs
@@ -1,22 +1,7 @@
 ﻿// Copyright (c) William Kent and contributors. All rights reserved.
 
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using WindowsLogViewer.Model;
 
