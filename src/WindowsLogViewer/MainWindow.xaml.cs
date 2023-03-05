bc3a017447b52718029f833bc4007aa3a3b91fbc
William Kent <wjk011@gmail.com>
2023-03-05 14:34:10 -0500

    Preliminary implementation of listing log entries

--- src/WindowsLogViewer/MainWindow.xaml.cs
+++ src/WindowsLogViewer/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) William Kent and contributors. All rights reserved.
 
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 
 using WindowsLogViewer.Model;
 
@@ -12,6 +14,22 @@ namespace LogViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private sealed class DataHolder : INotifyPropertyChanged
+        {
+            public ObservableCollection<BaseLogModelSource> Sources { get; } = new ObservableCollection<BaseLogModelSource>();
+
+            public BaseLogModelSource? CurrentSource { get; set; }
+
+            public List<LogModelEntry> Entries { get; } = new List<LogModelEntry>();
+
+            public void OnPropertyChanged(string name)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
@@ -22,21 +40,39 @@ namespace LogViewer
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            ObservableCollection<BaseLogModelSource> sources = new ObservableCollection<BaseLogModelSource>
-            {
-                ClassicLogModel.ApplicationLog,
-                ClassicLogModel.SecurityLog,
-                ClassicLogModel.SetupLog,
-                ClassicLogModel.SystemLog,
-            };
+            DataHolder holder = new DataHolder();
+            var sources = holder.Sources;
 
-            DataContext = sources;
+            sources.Add(ClassicLogModel.ApplicationLog);
+            sources.Add(ClassicLogModel.SecurityLog);
+            sources.Add(ClassicLogModel.SetupLog);
+            sources.Add(ClassicLogModel.SystemLog);
+
+            DataContext = holder;
 
             Task.Run(() =>
             {
                 foreach (var source in EtwLogModel.AllLogs)
                     Dispatcher.InvokeAsync(() => sources.Add(source));
             });
+        }
+
+        private void LogChooser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataHolder holder = (DataHolder)DataContext;
+            holder.CurrentSource = (BaseLogModelSource?)LogChooser.SelectedItem;
+            holder.OnPropertyChanged(nameof(holder.CurrentSource));
+
+            if (holder.CurrentSource != null)
+            {
+                holder.Entries.Clear();
+                holder.Entries.AddRange(holder.CurrentSource.Read(20));
+                holder.OnPropertyChanged(nameof(holder.Entries));
+            }
+            else
+            {
+                EventList.SetCurrentValue(ItemsControl.ItemsSourceProperty, null);
+            }
         }
     }
 }
