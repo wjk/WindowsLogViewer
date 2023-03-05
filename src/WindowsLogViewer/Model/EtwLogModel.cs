e78c71f082331217688bcf2895a2c36f2650be4f
William Kent <wjk011@gmail.com>
2023-03-05 15:27:01 -0500

    Fix copy/paste bug
    This would bite me shortly.

--- src/WindowsLogViewer/Model/EtwLogModel.cs
+++ src/WindowsLogViewer/Model/EtwLogModel.cs
@@ -128,6 +128,7 @@ internal sealed class EtwLogModel : BaseLogModelSource, IDisposable
                 Source = logEntry.ProviderName,
                 EventId = logEntry.Id,
                 Message = logEntry.FormatDescription(),
+                TimeStamp = logEntry.TimeCreated,
 
                 Severity = logEntry.LevelDisplayName switch
                 {
